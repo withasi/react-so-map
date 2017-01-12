3eaab14107f28afea052594fb2150d53ef83548c
ted <withasi@gmail.com>
2017-01-12 11:35:59 +0800

    for test

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 /**
  * Created by siwangli on 2016/12/11.
  */
-//import SoMap from './src/SoMap';
-//import Overlays from './src/Overlays';
-//export SerchBox from './src/SerchBox';
-//export DirectionBox from './src/DirectionBox';
-//export ControlMenu from './src/ControlMenu';
-//export {SoMap, Overlays};
+console.log(123)
 
 
 import React from "react";
